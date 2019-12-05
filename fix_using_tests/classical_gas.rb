a8ace754c5c3690dfd07ef95c06b401f3c2bf7d2
Cindy Kei <myk245@nyu.edu>
2019-12-05 02:51:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -11,8 +11,8 @@ class GasStation
     @unleaded_price = unleaded_price
   end
 
-end
-
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+
+end
